2a031fc70bfcbe142f4cf39806f96ab37622857d
Carl Olsen <unstoppablecarlolsen@gmail.com>
2013-07-09 09:01:36 -0400

    travis ci

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,9 @@
 <?php
 
+require_once __DIR__ . '/../src/Navinator/Collection.php';
+require_once __DIR__ . '/../src/Navinator/Node.php';
+require_once __DIR__ . '/../src/Navinator/Exception.php';
+
 //error_reporting(E_ALL | E_STRICT);
 //$loader = require_once __DIR__ . '/../vendor/autoload.php';
 //$loader->add('Navinator', __DIR__);
