6a5c2b08a3b8149c18837b1081182a38bc911436
Carl Olsen <unstoppablecarlolsen@gmail.com>
2013-07-10 12:05:10 -0400

    cleanup

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,9 +1,5 @@
 <?php
-
+//error_reporting(E_ALL | E_STRICT);
 require_once __DIR__ . '/../src/Navinator/Collection.php';
 require_once __DIR__ . '/../src/Navinator/Node.php';
 require_once __DIR__ . '/../src/Navinator/Exception.php';
-
-//error_reporting(E_ALL | E_STRICT);
-//$loader = require_once __DIR__ . '/../vendor/autoload.php';
-//$loader->add('Navinator', __DIR__);
