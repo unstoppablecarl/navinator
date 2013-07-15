1b83a501d41750619820b39b74e004bf816a0108
Carl Olsen <unstoppablecarlolsen@gmail.com>
2013-07-15 13:57:54 -0400

    cleanup phpunit bootstrap

--- tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-//error_reporting(E_ALL | E_STRICT);
-require_once __DIR__ . '/../src/Navinator/Collection.php';
-require_once __DIR__ . '/../src/Navinator/Node.php';
-require_once __DIR__ . '/../src/Navinator/StringHelper.php';
-require_once __DIR__ . '/../src/Navinator/Exception.php';
